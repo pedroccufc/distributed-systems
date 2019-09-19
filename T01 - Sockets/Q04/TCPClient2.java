f68e0b4b486852b9e0679a5001e8f3091dde5fac
pedroccufc <pedro_alves96@yahoo.com.br>
2019-09-18 23:19:45 -0300

    processo para cliente2 do chat

---
+++ T01 - Sockets/Q04/TCPClient2.java
@@ -0,0 +1,31 @@
+import java.net.*;
+import java.io.*;
+import java.util.Scanner;
+public class TCPClient2 {
+	public static void main (String args[]) {
+		// arguments supply message and hostname
+		Socket s = null;
+		try{
+			String msn = " ";
+			Scanner fluxo = new Scanner(System.in);
+			int serverPort = 7896;
+			s = new Socket("localhost", serverPort); 
+			DataInputStream in = new DataInputStream( s.getInputStream());
+			DataOutputStream out =new DataOutputStream( s.getOutputStream());
+			String data = " ";
+
+			while(true){
+				data = in.readUTF();
+				System.out.println("Cliente 1: " + data);
+				System.out.print("Cliente 2: ");
+				msn = fluxo.nextLine();
+				out.writeUTF(msn);
+			}
+			
+			
+		}catch (UnknownHostException e){System.out.println("Socket:"+e.getMessage());
+		}catch (EOFException e){System.out.println("EOF:"+e.getMessage());
+		}catch (IOException e){System.out.println("readline:"+e.getMessage());
+		}finally {if(s!=null) try {s.close();}catch (IOException e){System.out.println("close:"+e.getMessage());}}
+     }
+}
