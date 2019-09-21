8fb6fc44445abef6a7c00ddd37b9fceb2f70818a
pedroccufc <pedro_alves96@yahoo.com.br>
2019-09-21 17:18:40 -0300

    chat sincrono

--- T01 - Sockets/Q04/TCPClient1.java
+++ T01 - Sockets/Q04/TCPClient1.java
@@ -1,28 +1,28 @@
 import java.net.*;
 import java.io.*;
 import java.util.Scanner;
+
 public class TCPClient1 {
-	public static void main (String args[]) {
-		// arguments supply message and hostname
+	public static void main (String[] args) {
+                
+        Scanner fluxo = new Scanner(System.in);
 		Socket s = null;
+
 		try{
-			String msn = " ";
-			Scanner fluxo = new Scanner(System.in);
 			int serverPort = 7896;
-			s = new Socket("localhost", serverPort); 
+            InetAddress idnet = InetAddress.getByName("localhost");
+			s = new Socket(idnet, serverPort, idnet, 1287);
+
 			DataInputStream in = new DataInputStream( s.getInputStream());
 			DataOutputStream out =new DataOutputStream( s.getOutputStream());
-			String data = " ";
-
-			while(true){
-				System.out.print("Cliente 1: ");
-				msn = fluxo.nextLine();
-				out.writeUTF(msn);
-				data = in.readUTF();
-				System.out.println("Cliente 2: " + data);
-			}
-			
-			
+                        
+            String ler;                        
+            while(true){
+                ler = fluxo.nextLine();
+                out.writeUTF(ler);
+                String data = in.readUTF();
+                System.out.println("Cliente 2: "+ data) ;
+            } 
 		}catch (UnknownHostException e){System.out.println("Socket:"+e.getMessage());
 		}catch (EOFException e){System.out.println("EOF:"+e.getMessage());
 		}catch (IOException e){System.out.println("readline:"+e.getMessage());
