e5508824a78bcf04c30b2f343bcfa5b5e43392f6
pedroccufc <pedro_alves96@yahoo.com.br>
2019-09-21 17:20:59 -0300

    chat sincrono

--- T01 - Sockets/Q04/TCPServer.java
+++ T01 - Sockets/Q04/TCPServer.java
@@ -1,59 +1,53 @@
 import java.net.*;
 import java.io.*;
-import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class TCPServer {
-	public static void main (String args[]) {
-		try{
-			int serverPort = 7896; // the server port
-			ServerSocket listenSocket = new ServerSocket(serverPort);
-			while(true) {
-				Socket clientSocket1 = listenSocket.accept();
-				Socket clientSocket2 = listenSocket.accept();
-				Connection c = new Connection(clientSocket1, clientSocket2);
-			}
-		} catch(IOException e) {System.out.println("Listen socket:"+e.getMessage());}
-	}
-}
-class Connection extends Thread {
+    public static void main (String[] args) {
+        try{
+           
+            ServerSocket sv = new ServerSocket(7896);
 
-	// Cliente 01
-	DataInputStream in1;
-	DataOutputStream out1;
-	Socket clientSocket1;
+            Socket listenSocket = sv.accept();
+            Socket clientSocket = listenSocket;
+            System.out.println("Cliente 1: "+clientSocket.getPort());
 
-	// Cliente 02
-	DataInputStream in2;
-	DataOutputStream out2;
-	Socket clientSocket2;
-	
-	public Connection (Socket aClientSocket, Socket bClientSocket) {
-		try {
-			clientSocket1 = aClientSocket;
-			in1 = new DataInputStream(clientSocket1.getInputStream());
-			out1 = new DataOutputStream(clientSocket1.getOutputStream());
-
-			clientSocket2 = bClientSocket;
-			in2 = new DataInputStream(clientSocket2.getInputStream());
-			out2 = new DataOutputStream(clientSocket2.getOutputStream());
-
-			String msn1 = " ";
-			String msn2 = " ";
-
-			Scanner fluxo = new Scanner(System.in);
-			String data = " ";
-
-			while (true) {
-				msn1 = in1.readUTF();
-				if (!msn1.equals(null)) {
-					out2.writeUTF(msn1);
-				}
-
-				msn2 = in2.readUTF();
-				if (!msn2.equals(null)) {
-					out1.writeUTF(msn2);
-				}
-			}
-
-		} catch(IOException e) {System.out.println("Connection:"+e.getMessage());}
-	}
+            Socket listenSocket2 = sv.accept();
+            Socket clientSocket2 = listenSocket2;
+            System.out.println("Cliente 2: "+clientSocket2.getPort());
+            
+            DataInputStream in1;
+            DataOutputStream out1;
+    
+            DataInputStream in2;
+            DataOutputStream out2;
+            
+            in1 = new DataInputStream( clientSocket.getInputStream());
+            out1 =new DataOutputStream( clientSocket.getOutputStream());
+            
+            in2 = new DataInputStream( clientSocket2.getInputStream());
+            out2 =new DataOutputStream( clientSocket2.getOutputStream());
+            
+            String msn1 = "", msn2 = "";
+            
+            while(true){
+                msn1 = in1.readUTF();
+                if(msn1!=null){
+                  out2.writeUTF(msn1);
+                }
+                
+                msn2 = in2.readUTF();
+                if(msn2!=null){
+                  out1.writeUTF(msn2);
+                }
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(TCPServer.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            
+        } catch(IOException e) {System.out.println("Listen socket:"+e.getMessage());}
+    }
 }
