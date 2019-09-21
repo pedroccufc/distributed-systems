624d155bebeffb6ee10c66f7aed00f9da6de3308
pedroccufc <pedro_alves96@yahoo.com.br>
2019-09-21 16:15:04 -0300

    teste TCP

--- T01 - Sockets/Q01/TCP/TCPClient.java
+++ T01 - Sockets/Q01/TCP/TCPClient.java
@@ -1,18 +1,27 @@
 import java.net.*;
 import java.io.*;
+import java.util.Scanner;
+
 public class TCPClient {
-	public static void main (String args[]) {
-		// arguments supply message and hostname
+	public static void main (String[] args) {
+		// Para ler do teclado
+		Scanner fluxo = new Scanner(System.in);
 		Socket s = null;
 		try{
-			String teste = "teste";
+			// Porta na qual o servidor vai escutar
 			int serverPort = 7896;
-			s = new Socket("localhost", serverPort);    
+			s = new Socket("localhost", serverPort);
+
+			// para entrada e saida
 			DataInputStream in = new DataInputStream( s.getInputStream());
 			DataOutputStream out =new DataOutputStream( s.getOutputStream());
-			out.writeUTF(teste);      	// UTF is a string encoding see Sn. 4.4
-			String data = in.readUTF();	    // read a line of data from the stream
-			System.out.println("Received: "+ data) ; 
+            
+            // lendo a mensagem e enviando para o servidor
+			out.writeUTF(fluxo.nextLine());
+
+			// mostrando a resposta que o servidor retornou
+			System.out.println("Received: "+ in.readUTF());
+
 		}catch (UnknownHostException e){System.out.println("Socket:"+e.getMessage());
 		}catch (EOFException e){System.out.println("EOF:"+e.getMessage());
 		}catch (IOException e){System.out.println("readline:"+e.getMessage());
