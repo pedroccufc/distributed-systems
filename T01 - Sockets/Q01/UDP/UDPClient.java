a73e884607f3477d40f70ecb0f2f2d0e792624e5
pedroccufc <pedro_alves96@yahoo.com.br>
2019-09-21 16:17:47 -0300

    teste UDP

--- T01 - Sockets/Q01/UDP/UDPClient.java
+++ T01 - Sockets/Q01/UDP/UDPClient.java
@@ -1,22 +1,33 @@
 import java.net.*;
 import java.io.*;
+import java.util.Scanner;
+
 public class UDPClient{
-    public static void main(String args[]){ 
-		// args give message contents and destination hostname
+    public static void main(String[] args){ 
 		DatagramSocket aSocket = null;
+
 		try {
-			String teste = "teste";
-			aSocket = new DatagramSocket();    
-			byte [] m = teste.getBytes();
-			InetAddress aHost = InetAddress.getByName("localhost");
-			int serverPort = 6789;		                                                 
-			DatagramPacket request =
-			 	new DatagramPacket(m,  teste.length(), aHost, serverPort);
-			aSocket.send(request);			                        
-			byte[] buffer = new byte[1000];
-			DatagramPacket reply = new DatagramPacket(buffer, buffer.length);	
-			aSocket.receive(reply);
-			System.out.println("Reply: " + new String(reply.getData()));	
+            
+			aSocket = new DatagramSocket();   
+
+            // para ler do teclado
+            Scanner fluxo = new Scanner(System.in);
+            String ler = fluxo.nextLine();
+
+            byte[] m = ler.getBytes();
+            InetAddress aHost = InetAddress.getByName("localhost");
+
+            int serverPort = 6789;		                                                 
+            DatagramPacket request = new DatagramPacket(m, ler.length(), aHost, serverPort);
+            aSocket.send(request);
+
+            byte[] buffer = new byte[1000];
+
+            DatagramPacket reply = new DatagramPacket(buffer, buffer.length);	
+            aSocket.receive(reply);
+
+            System.out.println("Reply: " + new String(reply.getData()));
+
 		}catch (SocketException e){System.out.println("Socket: " + e.getMessage());
 		}catch (IOException e){System.out.println("IO: " + e.getMessage());
 		}finally {if(aSocket != null) aSocket.close();}
