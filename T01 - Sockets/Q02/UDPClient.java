b51dfae4165017b55c9c6eb4216704135c56eff9
pedroccufc <pedro_alves96@yahoo.com.br>
2019-09-21 16:20:25 -0300

    calculadora UDP

--- T01 - Sockets/Q02/UDPClient.java
+++ T01 - Sockets/Q02/UDPClient.java
@@ -1,27 +1,34 @@
 import java.net.*;
 import java.io.*;
 import java.util.Scanner;
+
 public class UDPClient{
-    public static void main(String args[]){ 
-		// args give message contents and destination hostname
+    public static void main(String[] args){ 
+        Scanner fluxo = new Scanner(System.in);
 		DatagramSocket aSocket = null;
+
 		try {
-			Scanner ler = new Scanner(System.in);
+			aSocket = new DatagramSocket();   
+            System.out.println("Digite a expressão matemática (EXEMPLO: 4+5): ");
 
-			System.out.println("Digite a equação:");
-			String str = ler.nextLine();
+            String exp = fluxo.nextLine();
+                        
+            if(exp.equals("")){
+                System.out.println("Mensagem vazia!");
+            } else {
+                byte [] m = exp.getBytes();
+                InetAddress aHost = InetAddress.getByName("localhost");
+                int serverPort = 6789;
 
-			aSocket = new DatagramSocket();    
-			byte [] m = str.getBytes();
-			InetAddress aHost = InetAddress.getByName("localhost");
-			int serverPort = 6789;		                                                 
-			DatagramPacket request =
-			 	new DatagramPacket(m,  str.length(), aHost, serverPort);
-			aSocket.send(request);			                        
-			byte[] buffer = new byte[1000];
-			DatagramPacket reply = new DatagramPacket(buffer, buffer.length);	
-			aSocket.receive(reply);
-			System.out.println("Reply: " + new String(reply.getData()));	
+                DatagramPacket request = new DatagramPacket(m, exp.length(), aHost, serverPort);
+                aSocket.send(request);
+
+                byte[] buffer = new byte[1000];
+                DatagramPacket reply = new DatagramPacket(buffer, buffer.length);	
+                aSocket.receive(reply);
+                System.out.println("Resultado: " + new String(reply.getData()));
+            }
+
 		}catch (SocketException e){System.out.println("Socket: " + e.getMessage());
 		}catch (IOException e){System.out.println("IO: " + e.getMessage());
 		}finally {if(aSocket != null) aSocket.close();}
