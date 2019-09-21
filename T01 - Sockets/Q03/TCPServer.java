547309a1a8acd9c89bf36f9e27b78dc5c7135740
pedroccufc <pedro_alves96@yahoo.com.br>
2019-09-21 16:58:54 -0300

    calculadora TCP

--- T01 - Sockets/Q03/TCPServer.java
+++ T01 - Sockets/Q03/TCPServer.java
@@ -1,81 +1,76 @@
 import java.net.*;
 import java.io.*;
+
 public class TCPServer {
-	public static void main (String args[]) {
-		try{
-			int serverPort = 7896; // the server port
+    
+	public static void main (String[] args) {
+        
+        try{
+			int serverPort = 7896;
 			ServerSocket listenSocket = new ServerSocket(serverPort);
+            Manager mg = new Manager();
+                        
 			while(true) {
 				Socket clientSocket = listenSocket.accept();
-				Connection c = new Connection(clientSocket);
+                if(!mg.verifica(clientSocket)) {
+					mg.clients.add(clientSocket);
+					mg.sinal.add('#');
+					mg.n1.add(0.0);
+					mg.n2.add(0.0);
+				}
+                int ind = mg.clients.indexOf(clientSocket);
+				Connection c = new Connection(clientSocket, mg, ind);
 			}
-		} catch(IOException e) {System.out.println("Listen socket:"+e.getMessage());}
+		} catch(IOException e) {System.out.println("Listen socket: "+e.getMessage());}
 	}
 }
+
 class Connection extends Thread {
 	DataInputStream in;
 	DataOutputStream out;
 	Socket clientSocket;
-	public Connection (Socket aClientSocket) {
+	int indice;
+	Manager manager;
+	
+	public Connection (Socket aClientSocket, Manager mg, int ind) {
 		try {
 			clientSocket = aClientSocket;
 			in = new DataInputStream( clientSocket.getInputStream());
 			out =new DataOutputStream( clientSocket.getOutputStream());
+			manager = mg; 
+			indice = ind;
 			this.start();
 		} catch(IOException e) {System.out.println("Connection:"+e.getMessage());}
 	}
 	public void run(){
-		try {			                 // an echo server
-
-			String data = in.readUTF();	 
-			int pos = 0;                 // read a line of data from the stream
-			char sinal = ' ';
-			double operacao = 0;
-			String resultado = " ";
-
-				for(int i=0; i < data.length(); i++){
-					if(data.charAt(i) == '+' || data.charAt(i) == '-' || data.charAt(i) == '*' || data.charAt(i) == '/'){
-						sinal = data.charAt(i);
-						pos = i;
-					}
-				}
-
-				String op1_ = data.substring(0, pos);
-				String op2_ = data.substring(pos+1, data.length());
-
-				double op1 = Double.parseDouble(op1_);
-				double op2 = Double.parseDouble(op2_);
-
-				switch(sinal){
-					case '+':
-						operacao = op1 + op2;
-						resultado = Double.toString(operacao);
-						break;
-					case '-':
-						operacao = op1 - op2;
-						resultado = Double.toString(operacao);
-						break;
-					case '*':
-						operacao = op1 * op2;
-						resultado = Double.toString(operacao);
-						break;
-					case '/':
-						if (op2 == 0) {
-							resultado = "Impossível dividir por 0";
-						} else {
-							operacao = op1 / op2;
-							resultado = Double.toString(operacao);
-						}
-						break;
-					default:
-						break;
-				}
-
-			out.writeUTF(resultado);
+		try {			                 
+            String data = in.readUTF();	                  
+            System.out.println("Expressão recebida: " + data);
+            if(data.equals("historico")){
+                Calculadora calc = new Calculadora();
+                String result = calc.Calcula(manager.n1.get(indice-1), manager.n2.get(indice-1), manager.sinal.get(indice-1));
+                out.writeUTF(Double.toString(manager.n1.get(indice-1))+Character.toString(manager.sinal.get(indice-1))+Double.toString(manager.n2.get(indice-1))+" = "+result);
+                //o que é passado para out é: n1 'sinal' n2 = result
+            } else {   
+                char sinal ='#';
+                int pos = 0;
+                for (int i = 0; i < data.length(); i++) {
+                    if(data.charAt(i) == '+' || data.charAt(i) == '-' || data.charAt(i) == '*' || data.charAt(i) == '/'){
+                        sinal = data.charAt(i);
+                        pos = i;
+                        break;
+                    }
+                }
+                manager.n1.add(indice, Double.parseDouble(data.substring(0, pos)));
+                manager.n2.add(indice, Double.parseDouble(data.substring(pos+1, data.length())));
+                manager.sinal.add(indice, sinal);
+                    
+                Calculadora calc = new Calculadora();
+                String result = calc.Calcula(manager.n1.get(indice), manager.n2.get(indice), manager.sinal.get(indice));
+                out.writeUTF(result);
+            }      
 		}catch (EOFException e){System.out.println("EOF:"+e.getMessage());
 		} catch(IOException e) {System.out.println("readline:"+e.getMessage());
 		} finally{ try {clientSocket.close();}catch (IOException e){/*close failed*/}}
-		
-
 	}
 }
